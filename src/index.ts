8202fa818d15896b1e352431e3485d7555724d59
Anartz Mugika Ledo <mugan86@gmail.com>
2021-05-23 21:03:03 +0200

    2.- Aplicando bordes establecerlo

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import pdf, { CreateOptions } from 'html-pdf';
+import pdf, { CreateOptions } from "html-pdf";
 
 const content = `
 <h1>Título en el PDF creado con el paquete html-pdf</h1>
@@ -6,14 +6,22 @@ const content = `
 `;
 
 const options: CreateOptions = {
-    "format": "A4",        // allowed units: A3, A4, A5, Legal, Letter, Tabloid
-    "orientation": "portrait", // portrait or landscape
+  format: "A4", // allowed units: A3, A4, A5, Legal, Letter, Tabloid
+  orientation: "portrait", // portrait or landscape
+  border: {
+    top: "50px", // default is 0, units: mm, cm, in, px
+    right: "2px",
+    bottom: "20px",
+    left: "15px",
+  },
 };
 
-pdf.create(content, options).toFile('./pdf/2-example-a4-p.pdf', function(err, res) {
-    if (err){
-        console.log(err);
+pdf
+  .create(content, options)
+  .toFile("./pdf/4-example-a4-p-border-top-50.pdf", function (err, res) {
+    if (err) {
+      console.log(err);
     } else {
-        console.log(res);
+      console.log(res);
     }
-});
+  });
